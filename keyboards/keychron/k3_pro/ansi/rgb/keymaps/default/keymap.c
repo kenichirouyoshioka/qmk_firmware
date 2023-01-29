1901adbe0e9e04e7664f228def49ea670b3d9739
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2023-01-30 08:46:07 +0900

    8:45 1/30 k3_pro

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/default/keymap.c
@@ -190,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_2] = LAYOUT_ansi_84(
      KC_GRV,   KC_F1,     KC_F2,      KC_F3,      KC_F4,     KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_F15,    C(G(KC_Q)),KC_F14,
      KC_ESC,   KC_1,      KC_2,       KC_3,       KC_4,      KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,              KC_F13,
-     KC_TAB,   TD(TD_MG), TD(CT_F679),ALT_T(KC_R),KC_F1,     TD(TD_KO),KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,              G(KC_EQL),
+     KC_TAB,   C(G(KC_Q)),TD(CT_F679),ALT_T(KC_R),KC_F1,     TD(TD_KO),KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,              G(KC_EQL),
      MO(_FN1), KC_LEAD,   TD(TD_XZ),  KC_WH_U,    TD(TD_BEA),KC_ESC,   KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,               G(KC_MINS),
      KC_LSFT,             KC_U,       TD(TD_MG),  KC_SLSH,   KC_V,     G(KC_J),  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,   KC_UP,     KC_F,
      MO(_FN2), KC_LCTL,   MT(MOD_LGUI,KC_L),                           KC_SPC,                                 KC_LGUI,  MO(_FN1),TO(LAYER_1),KC_LEFT,   KC_DOWN,   KC_RGHT),
