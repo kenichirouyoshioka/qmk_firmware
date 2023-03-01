30a616c0476bd4a13a3a9f36bc39b09cd8411fa2
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2023-03-01 09:07:01 +0900

    9:06 3/1 k6_pro

--- keyboards/keychron/k6_pro/ansi/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/default/keymap.c
@@ -62,7 +62,7 @@ void matrix_scan_user(void) {
             rgb_matrix_set_color_all(0x00, 0x90, 0xFF);
             break;
         case MAC_FN1:
-            rgb_matrix_set_color_all(0x00, 0x90, 0xFF);
+            rgb_matrix_set_color_all(0x00, 0x80, 0x00);
             break;
         case _FN1:
             rgb_matrix_set_color_all(0x80, 0x00, 0x80);
@@ -200,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [LAYER_3] = LAYOUT_ansi_68(
      KC_ESC,      KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC, C(G(KC_Q)),
-     KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS, XXXXXXX,
+     KC_TAB,      KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS, TO(LAYER_1),
 MT(MOD_LCTL,KC_K),KC_A,     KC_S,     KC_D,     TD(TD_GF),KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,  XXXXXXX,
 MT(MOD_LSFT,KC_Y),KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,              KC_P8,   XXXXXXX,
      MO(_FN3),    MT(MOD_LALT,KC_N), MT(MOD_LGUI,KC_L),             KC_GRV,                       KC_LGUI,  MO(_FN1), TO(LAYER_1),KC_P4,    KC_P2,   KC_P6),
