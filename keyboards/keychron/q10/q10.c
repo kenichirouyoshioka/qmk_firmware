cccc5e9c384e5303f32485dc7010ba459bb849ed
kenichirouyoshioka <kenichirou@mub.biglobe.ne.jp>
2023-08-20 10:25:55 +0900

    10:25 8/20 q10 and common files

--- keyboards/keychron/q10/q10.c
+++ keyboards/keychron/q10/q10.c
@@ -31,7 +31,7 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
         return false;
     }
     if (index == 0) {
-        default_layer_set(1UL << (active ? 2 : 0));
+        default_layer_set(1UL << (active ? 3 : 0));
     }
     return true;
 }
