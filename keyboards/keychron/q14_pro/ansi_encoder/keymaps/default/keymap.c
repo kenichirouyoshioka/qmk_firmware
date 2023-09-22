8d13142b6d361dafadd4f9d9f3559b4bcae334b0
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2023-09-22 09:09:28 +0900

    9:09 9/22 q14_pro

--- keyboards/keychron/q14_pro/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q14_pro/ansi_encoder/keymaps/default/keymap.c
@@ -291,7 +291,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_1] = LAYOUT_105_ansi(
     MO(_FN2),    KC_F13,           KC_F14,       KC_F15,       G(C(KC_Q)),KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   MO(_BT),            TG(LAYER_5),
     KC_F1,       MT(MOD_LGUI,KC_X),TD(TD_VL),    KC_P0,        KC_ESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_MUTE,
-    MO(_FN2),    KC_WH_U,          TD(TD_BEA),   ALT_T(KC_R),  KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_VOLU,
+    G(KC_J),    KC_WH_U,          TD(TD_BEA),   ALT_T(KC_R),  KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_VOLU,
     QK_LEAD,     G(KC_Z),          KC_ESC,                     KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_VOLD,
     TD(TD_F1112),TD(TD_SLH),       TD(TD_MG),     KC_SPC,      KC_LSFT,             KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     QK_LEAD,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
     KC_LSFT,                       KC_LCTL,                    MO(_FN1),  KC_LALT,            KC_LGUI,            KC_SPC,   MO(_FN4),           KC_RSFT,            MO(_FN1), QK_LEAD,            KC_LEFT,  KC_DOWN,  KC_RGHT),
