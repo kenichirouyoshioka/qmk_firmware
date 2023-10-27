52fd6863a1353e3991e149fb866c3116aee51ed9
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2023-10-27 15:46:00 +0900

    15:46 10/27 q11

--- keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
@@ -386,18 +386,18 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_1] = LAYOUT_ansi_91(
         MO(_FN1),   G(C(KC_Q)),KC_F1,         KC_F2,      KC_F3,      KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_F13,    KC_F14,     TG(LAYER_5),
-        KC_WH_U,    KC_ESC,    KC_1,          KC_2,       KC_3,       KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,               KC_MUTE,
-        KC_WH_D,    KC_TAB,    KC_Q,          KC_W,       KC_E,       KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS,               KC_VOLU,
+        C(KC_WH_U), KC_ESC,    KC_1,          KC_2,       KC_3,       KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,               KC_MUTE,
+        C(KC_WH_D), KC_TAB,    KC_Q,          KC_W,       KC_E,       KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS,               KC_VOLU,
         KC_F15,     KC_LCTL,   KC_A,          KC_S,       KC_D,       KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,                KC_VOLD,
-        QK_LEAD,    KC_LSFT,                  KC_Z,       KC_X,       KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,   KC_UP,
+        TG(LAYER_2),KC_LSFT,                  KC_Z,       KC_X,       KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,   KC_UP,
         MO(_FN4),   MO(_FN1),  KC_LALT,       XXXXXXX,    KC_LGUI,              KC_SPC,                        KC_RSFT,            MO(_FN4),  MO(_FN1), QK_LEAD,  KC_LEFT,   KC_DOWN,    KC_RGHT),
     
     [LAYER_2] = LAYOUT_ansi_91(
         MO(_FN2),   KC_TRNS,    KC_COMM,  KC_DOT,           KC_F3,       KC_F4,      KC_F5,     KC_F6,      KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12,   KC_TRNS,   KC_TRNS,   MO(_FN1),
         G(KC_LBRC), G(KC_K),    KC_LSFT,  KC_LBRC,          KC_RBRC,     KC_P4,      KC_P6,     KC_P8,      KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,   KC_BSPC,              KC_TRNS,
-        G(KC_RBRC), KC_TAB,     G(KC_J),  TD(TD_MG),        ALT_T(KC_F1),KC_R,       TD(TD_KO), KC_F13,     KC_F14,  KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC,  KC_BSLS,              KC_TRNS,
-        G(C(KC_Q)), MO(_FN1),   QK_LEAD,  TD(TD_XZ),        KC_WH_U,     TD(TD_BEA), KC_ESC,    KC_H,       KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,           KC_PENT,              KC_TRNS,
-        KC_F,       KC_LSFT,              MO(_FN4),         TD(TD_F1112),TD(TD_SLH), TD(TD_VL), KC_P0,      QK_LEAD, KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSFT,   KC_UP,
+        G(KC_RBRC), KC_TAB,     G(KC_J),  TD(TD_MG),        ALT_T(KC_F1),KC_R,       TD(TD_KO), C(KC_WH_U), KC_F13,  KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC,  KC_BSLS,              KC_TRNS,
+        KC_PENT,    MO(_FN1),   QK_LEAD,  TD(TD_XZ),        KC_WH_U,     TD(TD_BEA), KC_ESC,    C(KC_WH_D), KC_F14,  KC_K,     KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,               KC_TRNS,
+        TO(LAYER_1),KC_LSFT,              MO(_FN4),         TD(TD_F1112),TD(TD_SLH), TD(TD_VL), KC_P0,      KC_F,    KC_F15,   KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSFT,   KC_UP,
         MO(_FN4),   MO(_FN2),   KC_LALT,  KC_LCTL, MT(MOD_LGUI,KC_L),                KC_SPC,                         KC_TRNS,            KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LEFT,   KC_DOWN,    KC_RGHT),
 
     [LAYER_3] = LAYOUT_ansi_91(
@@ -467,9 +467,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [LAYER_1] = {ENCODER_CCW_CW(KC_WH_D, KC_WH_U),        ENCODER_CCW_CW(TG(LAYER_2), TG(LAYER_2))},
-    [LAYER_2] = {ENCODER_CCW_CW(KC_WH_D, KC_WH_U),        ENCODER_CCW_CW(TG(LAYER_3), TG(LAYER_3))},
-    [LAYER_3] = {ENCODER_CCW_CW(KC_WH_D, KC_WH_U),        ENCODER_CCW_CW(TO(LAYER_1), TO(LAYER_1))},
+    [LAYER_1] = {ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)),        ENCODER_CCW_CW(TG(LAYER_2), TG(LAYER_2))},
+    [LAYER_2] = {ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)),        ENCODER_CCW_CW(TG(LAYER_3), TG(LAYER_3))},
+    [LAYER_3] = {ENCODER_CCW_CW(C(KC_WH_D), C(KC_WH_U)),        ENCODER_CCW_CW(TO(LAYER_1), TO(LAYER_1))},
     [LAYER_4] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),        ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [LAYER_5] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU),        ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
     
