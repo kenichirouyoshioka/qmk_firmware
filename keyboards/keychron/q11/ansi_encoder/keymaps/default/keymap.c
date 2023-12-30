ce0a4158e3d6397b80bce89f990c8174aa2d8e3d
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2023-12-30 15:17:55 +0900

    15:17 12/30 q11, q10

--- keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/default/keymap.c
@@ -484,7 +484,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(LAYER_3),KC_TAB,     KC_Q,          KC_W,       KC_E,       KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,  KC_BSLS,               KC_VOLU,
         KC_HOME,    KC_LCTL,    KC_A,          KC_S,       KC_D,       KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,                KC_VOLD,
         KC_END,     KC_LSFT,                   KC_Z,       KC_X,       KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,   KC_UP,
-        MO(_FN4),   MO(_FN1),   KC_LALT,       XXXXXXX,    KC_LGUI,              KC_SPC,                        KC_RSFT,            MO(_FN1), KC_RCTL,   KC_RGUI,  KC_LEFT,   KC_DOWN,    KC_RGHT),
+        MO(_FN4),   MO(_FN1),   XXXXXXX,       KC_LALT,    KC_LGUI,              KC_SPC,                        KC_RSFT,            MO(_FN1), KC_RCTL,   KC_RGUI,  KC_LEFT,   KC_DOWN,    KC_RGHT),
     
     [LAYER_2] = LAYOUT_ansi_91(
         MO(_FN2),   KC_TRNS,    KC_F1,         KC_F2,            KC_F3,       KC_F4,      KC_F5,      KC_F6,      KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12,   KC_TRNS,   KC_TRNS,   MO(_FN1),
@@ -492,7 +492,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,    TD(TD_TAB1),KC_LBRC,       KC_RBRC,          ALT_T(KC_F1),CTL_T(KC_R),TD(TD_1U),  KC_Y,       KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, KC_RBRC,  KC_BSLS,              KC_TRNS,
         G(KC_RBRC), MO(_FN1),   QK_LEAD,       TD(TD_XG),        KC_WH_U,     TD(TD_BEA), KC_ESC,     KC_H,       KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,               KC_TRNS,
         G(KC_LBRC), SFT_T(KC_M),               KC_F9,            TD(TD_F1112),TD(TD_KOH), TD(TD_VF8), TD(TD_02),  KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,           KC_RSFT,   KC_UP,
-        MO(_FN4),   MO(_FN2),   ALT_T(KC_SLSH),XXXXXXX,          GUI_T(KC_L),             KC_SPC,                          KC_TRNS,            KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LEFT,   KC_DOWN,    KC_RGHT),
+        MO(_FN4),   MO(_FN2),   XXXXXXX,       ALT_T(KC_SLSH),   GUI_T(KC_L),             KC_SPC,                          KC_TRNS,            KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LEFT,   KC_DOWN,    KC_RGHT),
 
     [LAYER_3] = LAYOUT_ansi_91(
         TO(LAYER_1),KC_TRNS,          KC_F1,      KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,  KC_F12,   KC_TRNS,   KC_TRNS,  MO(_FN1),
@@ -524,7 +524,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,   TG(LAYER_2), XXXXXXX,    G(KC_N),   G(KC_E),   G(KC_COMM),G(KC_T),   XXXXXXX,    XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,            XXXXXXX,
         KC_PGUP,   XXXXXXX,     G(KC_K),    G(KC_S),   G(KC_D),   SGUI(KC_Z),G(KC_G),   KC_LEFT,    KC_DOWN,   KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,              KC_ENT,             XXXXXXX,
         KC_PGDN,   XXXXXXX,                 XXXXXXX,   XXXXXXX,   G(KC_J),   XXXXXXX,   XXXXXXX,    XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,              KC_RSFT,  KC_VOLU,
-        XXXXXXX,   XXXXXXX,     G(KC_MINS), XXXXXXX,   G(KC_EQL),            KC_BSPC,                          KC_RSFT,            KC_RALT,  KC_RCTL,    KC_RGUI,  KC_MUTE,  KC_VOLD,  KC_MPLY),
+        XXXXXXX,   XXXXXXX,     XXXXXXX,    G(KC_MINS),G(KC_EQL),            KC_BSPC,                          KC_RSFT,            KC_RALT,  KC_RCTL,    KC_RGUI,  KC_MUTE,  KC_VOLD,  KC_MPLY),
     
     [_FN2] = LAYOUT_ansi_91(
         XXXXXXX,   XXXXXXX,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
