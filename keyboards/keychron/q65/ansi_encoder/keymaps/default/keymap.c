ef41e4cc9cddff93a9665c68a2884e4b297de122
kenichirouyoushioka <kenichirou@mub.biglobe.ne.jp>
2024-04-03 13:31:21 +0900

    13:31 4/3 q65

--- keyboards/keychron/q65/ansi_encoder/keymaps/default/keymap.c
+++ keyboards/keychron/q65/ansi_encoder/keymaps/default/keymap.c
@@ -703,9 +703,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LAYER_5] = LAYOUT_ansi_73(
         _______,     RO,         NU,       HU,       A,       U,       E,       O,       YA,      YU,      YO,      WA,       HO,       HANDAKU,  KC_BSPC,          TO(LAYER_1),
         KC_ESC,      KC_TAB,     TA,       TE,       I,       SU,      KA,      NN,      NA,      NI,      RA,      SE,       DAKUTEN,  MU,       HE,               KC_MUTE,
-        KC_GRV,      MO(_FN6),   TI,       TO,       SI,      HA,      KI,      KU,      MA,      NO,      RI,      RE,       KE,                 KC_ENT,           KC_VOLU,
+        KC_GRV,      MO(_FN5),   TI,       TO,       SI,      HA,      KI,      KU,      MA,      NO,      RI,      RE,       KE,                 KC_ENT,           KC_VOLU,
         _______,     MO(_FN4),             TU,       SA,      SO,      HI,      KO,      MI,      MO,      NE,      RU,       ME,                 MO(_FN4),KC_UP,   KC_VOLD,
-        _______,     MO(LAYER_6),KC_LALT,  KC_LGUI,                             KC_SPC,                             G(KC_SPC),MO(_FN6), KC_RGUI,  KC_LEFT, KC_DOWN, KC_RGHT),
+        _______,     MO(LAYER_6),KC_LALT,  KC_LGUI,                             KC_SPC,                             G(KC_SPC),MO(_FN5), KC_RGUI,  KC_LEFT, KC_DOWN, KC_RGHT),
 
     [LAYER_6] = LAYOUT_ansi_73(
         _______,     KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          TG(LAYER_5),
